48f125fdd14fda26e2b703bb71a5d82f2b3aa755
Kevin Thomas Manjooran <kevinthomastm2002@gmail.com>
2023-06-24 15:06:31 +0530

    adding database

--- spring-boot-example/src/main/java/com/kevinscode/Main.java
+++ spring-boot-example/src/main/java/com/kevinscode/Main.java
@@ -1,13 +1,12 @@
 package com.kevinscode;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 
 @SpringBootApplication
 @RestController
